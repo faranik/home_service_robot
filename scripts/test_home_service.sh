e0f88dd7dedd80632de8c9acdf747275caece991
Victor Olaru <olaruvictor.ov@gmail.com>
2019-05-02 10:52:36 -0700

    Add the world file and the map to repository.

--- scripts/test_home_service.sh
+++ scripts/test_home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/home_service_robot/map/playground.world " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch " & 
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/catkin_ws/src/home_service_robot/map/playground.yaml" & 
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/catkin_ws/src/home_service_robot/map/playground.world " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " & 
 sleep 5
